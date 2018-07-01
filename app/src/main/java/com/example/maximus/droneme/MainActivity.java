bf85bb37ce777231f5cadc13d69efb9ba62d782d
Maximilian Meyer <maximilian.mey@googlemail.com>
2018-07-01 15:34:44 +0200

    initialized fragments and started implementing them

--- app/src/main/java/com/example/maximus/droneme/MainActivity.java
+++ app/src/main/java/com/example/maximus/droneme/MainActivity.java
@@ -2,21 +2,33 @@ package com.example.maximus.droneme;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private Context context;
+public class MainActivity extends AppCompatActivity implements TransportFragment.OnFragmentInteractionListener, HomeFragment.OnFragmentInteractionListener {
+
     private TextView mTextMessage;
 
     //Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //
+    }
+
+    public void replaceFragment(View view) {
+        NavigationFragment blankFragment2 = new NavigationFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,blankFragment2).addToBackStack(null).commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -54,6 +66,20 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setIcon(R.drawable.ic_navigation);
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         actionBar.setCustomView(R.layout.top_nav_layout);
+
+        // initialize fragment view
+        if (findViewById(R.id.fragment_container) != null) {
+            System.out.println("Entered if");
+            if (savedInstanceState != null) {
+                System.out.println("There is a saved state. Returning!");
+                return;
+            }
+            HomeFragment homeFragment = new HomeFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, homeFragment).commit();
+        }
+        System.out.println("can we print tho");
+        System.out.println(R.id.fragment_container);
+
 
         setContentView(R.layout.activity_main);
         mTextMessage = (TextView) findViewById(R.id.message);
