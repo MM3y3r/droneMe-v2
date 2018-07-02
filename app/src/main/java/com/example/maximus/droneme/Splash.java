e0ea409cb2eb9191fde22b095358d0b8c2d4c1a8
LongAfterMidnight <eva.geiger91@gmail.com>
2018-07-02 09:29:04 +0200

    splash screen

---
+++ app/src/main/java/com/example/maximus/droneme/Splash.java
@@ -0,0 +1,22 @@
+package com.example.maximus.droneme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        iv = (ImageView) findViewById(R.id.iv) ;
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        iv.startAnimation(myanim);
+
+    }
+}
